f4d259505adfd4887ffbaaa93cfd4839b083b723
benapart <164779300+benapart@users.noreply.github.com>
2024-04-25 14:55:07 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,10 +8,15 @@ basic.forever(function () {
         basic.showIcon(IconNames.Yes)
         SuperBit.Servo2(SuperBit.enServo.S1, 90)
         music.play(music.stringPlayable("D F A C5 - - - - ", 150), music.PlaybackMode.UntilDone)
-        basic.pause(5000)
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
+        basic.pause(2000)
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
         SuperBit.Servo2(SuperBit.enServo.S1, 0)
         music.play(music.stringPlayable("C5 A F D - - - - ", 150), music.PlaybackMode.UntilDone)
     } else {
         basic.showIcon(IconNames.No)
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
+        basic.pause(2000)
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
     }
 })
